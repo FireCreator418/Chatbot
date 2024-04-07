01b836ee4935a61c289ce36841c42a527a9a73f1
FireCreator418 <165873494+FireCreator418@users.noreply.github.com>
2024-04-07 19:45:01 +1000

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ") # Your name input
+print("Hello", name)
+print("Im ChatBot!")
+wanaBeFrends = input("Wanna be Friends? ")
+if wanaBeFrends == "Yes":
+    favFood = input("What's your favoute food? ")
+    print("Mine is Fish!")
+    favCol = input("What's your favoute Color? ")
+    print("Mine is Blue!")
+    favMonth = input("What's your favoute Month? ")
+    print("Mine is May!")
+    print("Bye bye", name)
+if wanaBeFrends == "No":
+    print("Well then...")
